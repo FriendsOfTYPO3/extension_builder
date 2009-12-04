759a886ca008fe1681988b437379ddd9b78e34ae
Sebastian Michaelsen <sebastian.gebhard@googlemail.com>
2009-12-04 19:44:06 +0000

    [~BUGFIX] Extbase Kickstarter: SQL field names dont respect camelcase names, fixes #5018

--- Classes/Domain/Model/Property/FloatProperty.php
+++ Classes/Domain/Model/Property/FloatProperty.php
@@ -40,7 +40,7 @@ class Tx_ExtbaseKickstarter_Domain_Model_Property_FloatProperty extends Tx_Extba
 		return '';
 	}
 	public function getSqlDefinition() {
-		return strtolower($this->getName()) . " double(11,2) DEFAULT '0.00' NOT NULL,";
+		return $this->getName() . " double(11,2) DEFAULT '0.00' NOT NULL,";
 	}
 }
 ?>
