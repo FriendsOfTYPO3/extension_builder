825987c5c503c0539f48684d9dddce68144ed235
Sebastian Kurfürst <sebastian@typo3.org>
2010-02-08 22:21:56 +0000

    * Made ScaffoldingController and ScaffoldingView work (currently only for IndexAction)

--- Classes/Scaffolding/AbstractScaffoldingController.php
+++ Classes/Scaffolding/AbstractScaffoldingController.php
@@ -2,7 +2,7 @@
 /* 
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
- */
+*/
 class Tx_ExtbaseKickstarter_Scaffolding_AbstractScaffoldingController extends Tx_Extbase_MVC_Controller_ActionController {
 
 	/**
@@ -11,18 +11,74 @@ class Tx_ExtbaseKickstarter_Scaffolding_AbstractScaffoldingController extends Tx
 	protected $repository;
 
 	/**
+	 * Name of the Domain Object this Controller should provide CRUD functionality for. Example: "Blog".
+	 *
+	 * If not set, this is divised from the name of the Controller
+	 * (by using the last part of the controller class name without "Controller")
+	 *
 	 * @api
 	 * @var string
 	 */
-	protected $className = "Blog";
+	protected $domainObjectName = NULL;
 
-	protected $fullClassName; // TODO
+	/**
+	 * Full class name of the Domain Object this controller should provide CRUD functionality for.
+	 * Example: "Tx_BlogExample_Domain_Model_Blog"
+	 *
+	 * If not set, this is divised from $domainObjectName and $extensionName.
+	 * 
+	 * @var string
+	 */
+	protected $domainObjectClassName = NULL;
 
-	public function initializeAction() {
-		// TODO: Fill $this->className
-		$this->lcfirst(Tx_ExtbaseKickstarter_Utility_Inflector::pluralize($this->className)); // TODO: lcfirst >= php 5.3
-		
-		
+	/**
+	 *
+	 * @var string
+	 */
+	protected $repositoryClassName = NULL;
+
+
+	public function __construct() {
+		parent::__construct();
+		$this->setNameOfAssociatedDomainObject();
+	}
+
+	protected function setNameOfAssociatedDomainObject() {
+		$controllerClassName = get_class($this);
+
+		if ($this->domainObjectName === NULL) {
+			$this->domainObjectName = substr(array_pop(explode('_', $controllerClassName)), 0, - strlen('Controller'));
+		}
+
+		if ($this->domainObjectClassName === NULL) {
+			$this->domainObjectClassName = 'Tx_' . $this->extensionName . '_Domain_Model_' . $this->domainObjectName;
+		}
+
+		if ($this->repositoryClassName === NULL) {
+			$this->repositoryClassName = 'Tx_' . $this->extensionName . '_Domain_Repository_' . $this->domainObjectName . 'Repository';
+			// TODO: Exceptions if classes not found.
+		}
+
+		$this->repository = t3lib_div::makeInstance($this->repositoryClassName);
+	}
+
+	protected function resolveViewObjectName() {
+		// TODO: Return this only on list of supported views
+		return 'Tx_ExtbaseKickstarter_Scaffolding_ScaffoldingView';
+	}
+
+	/**
+	 * Initializes the view before invoking an action method.
+	 *
+	 * @param Tx_Extbase_View_ViewInterface $view The view to be initialized
+	 * @return void
+	 * @api
+	 */
+	protected function initializeView(Tx_Extbase_MVC_View_ViewInterface $view) {
+		if ($view instanceof Tx_ExtbaseKickstarter_Scaffolding_ScaffoldingView) {
+			$view->setDomainObjectClassName($this->domainObjectClassName);
+			$view->setDomainObjectName($this->domainObjectName);
+		}
 	}
 
 	/**
@@ -31,12 +87,13 @@ class Tx_ExtbaseKickstarter_Scaffolding_AbstractScaffoldingController extends Tx
 	 * @return string The rendered view
 	 */
 	public function indexAction() {
-		$this->view->assign('blogs', $this->blogRepository->findAll());
+		$lowercasedAndPluralizedDomainObjectName = Tx_ExtbaseKickstarter_Utility_Inflector::pluralize(lcfirst($this->domainObjectName)); // TODO: lcfirst >= php 5.3
+		$this->view->assign($lowercasedAndPluralizedDomainObjectName, $this->repository->findAll());
 	}
 
 
 	public function initializeNewAction() {
-		$this->arguments->addNewArgument('new' . $this->className, $this->fullClassName, FALSE);
+		$this->arguments->addNewArgument('new' . $this->domainObjectName, $this->domainObjectClassName, FALSE);
 		// Argument IS NOT VALIDATED (but that is correct)
 	}
 
@@ -46,7 +103,7 @@ class Tx_ExtbaseKickstarter_Scaffolding_AbstractScaffoldingController extends Tx
 	 * @return string An HTML form for creating a new blog
 	 */
 	public function newAction() {
-		$this->view->assign('new' . $this->className, $this->arguments['new' . $this->className]);
+		$this->view->assign('new' . $this->domainObjectName, $this->arguments['new' . $this->domainObjectName]);
 	}
 
 	/**
---
+++ Classes/Scaffolding/ScaffoldingView.php
@@ -0,0 +1,96 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+*/
+class Tx_ExtbaseKickstarter_Scaffolding_ScaffoldingView extends Tx_Fluid_View_TemplateView {
+
+	/**
+	 * @var Tx_ExtbaseKickstarter_Service_CodeGenerator
+	 */
+	protected $codeGenerator;
+
+	protected $domainObjectClassName;
+
+	protected $domainObjectName;
+
+	/**
+	 *
+	 * @var Tx_Extbase_Reflection_Service
+	 */
+	protected $reflectionService;
+
+	public function __construct() {
+		parent::__construct();
+		$this->codeGenerator = t3lib_div::makeInstance('Tx_ExtbaseKickstarter_Service_CodeGenerator');
+		$this->reflectionService = t3lib_div::makeInstance('Tx_Extbase_Reflection_Service');
+	}
+
+	public function setDomainObjectClassName($domainObjectClassName) {
+		$this->domainObjectClassName = $domainObjectClassName;
+	}
+	public function setDomainObjectName($domainObjectName) {
+		$this->domainObjectName = $domainObjectName;
+	}
+
+
+
+	protected function resolveTemplatePathAndFilename($actionName = NULL) {
+		$actionName = ($actionName !== NULL ? $actionName : $this->controllerContext->getRequest()->getControllerActionName());
+		$actionName = strtolower($actionName);
+
+		return $actionName;
+	}
+	protected function parseTemplate($templatePathAndFilename) {
+		if ($templatePathAndFilename == 'index') {
+			$domainObject = $this->buildDomainObjectByReflection();
+			$action = new Tx_ExtbaseKickstarter_Domain_Model_Action();
+			$action->setName('list');
+			$template = $this->codeGenerator->generateDomainTemplate($domainObject, $action);
+			return $this->templateParser->parse($template);
+		}
+
+		return null; // TODO: Should not happen!
+	}
+
+	/**
+	 * @return Tx_ExtbaseKickstarter_Domain_Model_DomainObject
+	 */
+	protected function buildDomainObjectByReflection() {
+		$domainObject = new Tx_ExtbaseKickstarter_Domain_Model_DomainObject();
+		$domainObject->setName($this->domainObjectName);
+		$classSchema = $this->reflectionService->getClassSchema($this->domainObjectClassName);
+
+		foreach ($classSchema->getProperties() as $propertyName => $propertyDescription) {
+
+			$propertyType = 'Tx_ExtbaseKickstarter_Domain_Model_Property_' . $this->resolveKickstarterPropertyTypeFromPropertyDescription($propertyDescription['type'], $propertyDescription['elementType']);
+			$property = new $propertyType;
+			$property->setName($propertyName);
+			$domainObject->addProperty($property);
+		}
+
+		return $domainObject;
+	}
+
+
+	/**
+	 * See Tx_Extbase_Reflection_ClassSchema::ALLOWED_TYPES_PATTERN
+	 */
+	protected function resolveKickstarterPropertyTypeFromPropertyDescription($phpPropertyType, $elementType) {
+		switch ($propertyDescription) {
+			case 'integer': return 'IntegerProperty';
+			case 'float': return 'FloatProperty';
+			case 'boolean': return 'BooleanProperty';
+			case 'string' : return 'StringProperty';
+			case 'DateTime': return 'DateTimeProperty';
+			case 'array':
+			case 'ArrayObject':
+			case 'Tx_Extbase_Persistence_ObjectStorage':
+				return 'Relation_ZeroToManyRelation'; // TODO: Is this correct?
+			default:
+			// Tx_*
+				return 'Relation_ZeroToOneRelation';
+		}
+	}
+}
+?>
