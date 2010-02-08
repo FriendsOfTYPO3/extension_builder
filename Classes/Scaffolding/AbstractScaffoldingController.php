1ea69766745a59d1000f057a1a1322e68d202ec6
Sebastian Kurfürst <sebastian@typo3.org>
2010-02-08 22:22:47 +0000

    * Updating documentation

--- Classes/Scaffolding/AbstractScaffoldingController.php
+++ Classes/Scaffolding/AbstractScaffoldingController.php
@@ -4,12 +4,6 @@
  * and open the template in the editor.
 */
 class Tx_ExtbaseKickstarter_Scaffolding_AbstractScaffoldingController extends Tx_Extbase_MVC_Controller_ActionController {
-
-	/**
-	 * @var Tx_Extbase_Persistence_RepositoryInterface
-	 */
-	protected $repository;
-
 	/**
 	 * Name of the Domain Object this Controller should provide CRUD functionality for. Example: "Blog".
 	 *
@@ -32,17 +26,33 @@ class Tx_ExtbaseKickstarter_Scaffolding_AbstractScaffoldingController extends Tx
 	protected $domainObjectClassName = NULL;
 
 	/**
+	 * Class name of the repository to be used. If NULL, we derive it from the $domainObjectName
 	 *
 	 * @var string
 	 */
 	protected $repositoryClassName = NULL;
 
+	/**
+	 * Repository
+	 *
+	 * @var Tx_Extbase_Persistence_RepositoryInterface
+	 */
+	protected $repository;
 
+	/**
+	 * Constructor.
+	 */
 	public function __construct() {
 		parent::__construct();
 		$this->setNameOfAssociatedDomainObject();
 	}
 
+	/**
+	 * Set the name of associated domain object, based on the controller
+	 * name.
+	 * 
+	 * @return void
+	 */
 	protected function setNameOfAssociatedDomainObject() {
 		$controllerClassName = get_class($this);
 
@@ -58,10 +68,23 @@ class Tx_ExtbaseKickstarter_Scaffolding_AbstractScaffoldingController extends Tx
 			$this->repositoryClassName = 'Tx_' . $this->extensionName . '_Domain_Repository_' . $this->domainObjectName . 'Repository';
 			// TODO: Exceptions if classes not found.
 		}
+	
+		if (!class_exists($this->domainObjectClassName)) {
+			throw new Exception('The domain object "' . $this->domainObjectClassName . '" does not exist.');
+		}
+		
+		if (!class_exists($this->repositoryClassName)) {
+			throw new Exception('The repository class "' . $this->domainObjectClassName . '" does not exist.');
+		}
 
 		$this->repository = t3lib_div::makeInstance($this->repositoryClassName);
 	}
 
+	/**
+	 * Object name of view.
+	 * 
+	 * @return string
+	 */
 	protected function resolveViewObjectName() {
 		if (class_exists(parent::resolveViewObjectName())) {
 			return parent::resolveViewObjectName();
@@ -75,7 +98,6 @@ class Tx_ExtbaseKickstarter_Scaffolding_AbstractScaffoldingController extends Tx
 	 *
 	 * @param Tx_Extbase_View_ViewInterface $view The view to be initialized
 	 * @return void
-	 * @api
 	 */
 	protected function initializeView(Tx_Extbase_MVC_View_ViewInterface $view) {
 		if ($view instanceof Tx_ExtbaseKickstarter_Scaffolding_ScaffoldingView) {
@@ -85,7 +107,11 @@ class Tx_ExtbaseKickstarter_Scaffolding_AbstractScaffoldingController extends Tx
 	}
 
 	/**
-	 * Index action for this controller. Displays a list of blogs.
+	 * SECTION: CRUD Actions
+	 */
+
+	/**
+	 * Index action for this controller. Displays a list of domain objects.
 	 *
 	 * @return string The rendered view
 	 */
@@ -94,7 +120,11 @@ class Tx_ExtbaseKickstarter_Scaffolding_AbstractScaffoldingController extends Tx
 		$this->view->assign($lowercasedAndPluralizedDomainObjectName, $this->repository->findAll());
 	}
 
-
+	/**
+	 * Initialize the NEW Action; Adds a parameter without validation.
+	 *
+	 * @return void
+	 */
 	public function initializeNewAction() {
 		$this->arguments->addNewArgument('new' . $this->domainObjectName, $this->domainObjectClassName, FALSE);
 	}
@@ -108,10 +138,16 @@ class Tx_ExtbaseKickstarter_Scaffolding_AbstractScaffoldingController extends Tx
 		$this->view->assign('new' . $this->domainObjectName, $this->arguments['new' . $this->domainObjectName]->getValue());
 	}
 
+	/**
+	 * Initialize the "CREATE" action which should actually CREATE the object.
+	 *
+	 * @return void
+	 */
 	public function initializeCreateAction() {
 		$argument = $this->arguments->addNewArgument('new' . $this->domainObjectName, $this->domainObjectClassName, TRUE);
 		$argument->setValidator($this->validatorResolver->getBaseValidatorConjunction($this->domainObjectClassName));
 	}
+	
 	/**
 	 * Creates a new domain object
 	 *
@@ -123,12 +159,17 @@ class Tx_ExtbaseKickstarter_Scaffolding_AbstractScaffoldingController extends Tx
 		$this->redirect('index');
 	}
 
+	/**
+	 * Initialize the EDIT action which displays the edit form.
+	 *
+	 * @return void
+	 */
 	public function initializeEditAction() {
 		$this->arguments->addNewArgument(lcfirst($this->domainObjectName), $this->domainObjectClassName, FALSE);
 	}
 
 	/**
-	 * Edits an existing domain object
+	 * Displays an EDIT form for an existing domain object
 	 *
 	 * @return string Form for editing the existing object
 	 */
@@ -136,13 +177,18 @@ class Tx_ExtbaseKickstarter_Scaffolding_AbstractScaffoldingController extends Tx
 		$this->view->assign(lcfirst($this->domainObjectName), $this->arguments[lcfirst($this->domainObjectName)]->getValue());
 	}
 
+	/**
+	 * Initialize the UPDATE action, which actually UPDATES the object.
+	 *
+	 * @return void
+	 */
 	public function initializeUpdateAction() {
 		$argument = $this->arguments->addNewArgument(lcfirst($this->domainObjectName), $this->domainObjectClassName, TRUE);
 		$argument->setValidator($this->validatorResolver->getBaseValidatorConjunction($this->domainObjectClassName));
 	}
 
 	/**
-	 * Updates an existing domain object
+	 * Updates an existing domain object.
 	 *
 	 * @return void
 	 */
@@ -152,6 +198,11 @@ class Tx_ExtbaseKickstarter_Scaffolding_AbstractScaffoldingController extends Tx
 		$this->redirect('index');
 	}
 
+	/**
+	 * Initializes the DELETE action.
+	 *
+	 * @return void
+	 */
 	public function initializeDeleteAction() {
 		$this->arguments->addNewArgument(lcfirst($this->domainObjectName), $this->domainObjectClassName, TRUE);
 	}
@@ -166,6 +217,5 @@ class Tx_ExtbaseKickstarter_Scaffolding_AbstractScaffoldingController extends Tx
 		$this->flashMessages->add('Your ' . $this->domainObjectName . ' has been removed.');
 		$this->redirect('index');
 	}
-
 }
 ?>
