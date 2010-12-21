be9f6baa881cb9c459c4628f2e246c564d8acab9
Nico de Haen <mail@ndh-websolutions.de>
2010-12-21 00:32:40 +0000

    Oversaw a test file...

---
+++ Tests/BaseTestCase.php
@@ -0,0 +1,57 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 Jochen Rau <jochen.rau@typoplanet.de>
+*  All rights reserved
+*
+*  This class is a backport of the corresponding class of FLOW3.
+*  All credits go to the v5 team.
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+if (!class_exists('Tx_Extbase_Utility_ClassLoader')) {
+	require(t3lib_extmgm::extPath('extbase') . 'Classes/Utility/ClassLoader.php');
+}
+
+$classLoader = new Tx_Extbase_Utility_ClassLoader();
+spl_autoload_register(array($classLoader, 'loadClass'));
+require_once(t3lib_extmgm::extPath('extbase') . 'Tests/BaseTestCase.php');
+
+abstract class Tx_ExtbaseKickstarter_BaseTestCase extends Tx_Extbase_BaseTestCase {
+	
+	/**
+	 * Helper function
+	 * @param $name
+	 * @param $entity
+	 * @param $aggregateRoot
+	 * @return object Tx_ExtbaseKickstarter_Domain_Model_DomainObject
+	 */
+	protected function buildDomainObject($name, $entity = false, $aggregateRoot = false){
+		$domainObject = new Tx_ExtbaseKickstarter_Domain_Model_DomainObject();
+		$domainObject->setExtension($this->extension);
+		$domainObject->setName($name);
+		$domainObject->setEntity($entity);
+		$domainObject->setAggregateRoot($aggregateRoot);
+		return $domainObject;
+	}
+
+}
+
+
+
+
