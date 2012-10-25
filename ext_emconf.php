50de675a27ef4fcc1991faa620b35854de48fcf9
Roland Waldner <most.wanted@gmx.at>
2012-10-25 20:03:21 +0200

    [TASK] Fix CGL violations InlineCommentWrongStyle
    Change-Id: I23380a2c69fdddd32613501f3648ed7df52f7bfb
    Resolves: #42365

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "extension_builder".
-#
-# Auto generated 01-07-2012 14:18
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "extension_builder".
+ *
+ * Auto generated 01-07-2012 14:18
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Extension Builder',
