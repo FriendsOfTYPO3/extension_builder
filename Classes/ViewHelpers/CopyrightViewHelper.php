c73b533adce92f96edb91a8f20550e450d6b85cc
Andreas Lappe <nd@off-pist.de>
2013-07-24 17:20:54 +0200

    [TASK] Format copyright holders with ViewHelper
    Resolves: #50400
    Change-Id: I4d03c83017f1f40dd1d9c0a6496f309251b7b73b
    Reviewed-on: https://review.typo3.org/22540
    Reviewed-by: Nico de Haen
    Tested-by: Nico de Haen

---
+++ Classes/ViewHelpers/CopyrightViewHelper.php
@@ -0,0 +1,65 @@
+<?php
+/*                                                                        *
+ * This script belongs to the TYPO3 package "Extension Builder".          *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * Format the Copyright notice
+ *
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ * @scope prototype
+ */
+class Tx_ExtensionBuilder_ViewHelpers_CopyrightViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+	/**
+	 * Format the copyright holder's name(s)
+	 *
+	 * @param string $date
+	 * @param array $persons
+	 * @return string The copyright ownership
+	 * @author Andreas Lappe <nd@kaeufli.ch>
+	 */
+	public function render($date, $persons) {
+		$copyright= ' *  (c) ' . $date . ' ';
+		$offset = strlen($copyright) - 2;
+
+		foreach ($persons as $index => $person) {
+			$entry = '';
+
+			if ($index !== 0) {
+				$entry .= chr(10) . ' *' . str_repeat(' ', $offset);
+			}
+
+			$entry .= $person->getName();
+
+			if ($person->getEmail() !== '') {
+				$entry .= ' <' . $person->getEmail() . '>';
+			}
+
+			if ($person->getCompany() !== '') {
+				$entry .= ', ' . $person->getCompany();
+			}
+
+			$copyright .= $entry;
+		}
+
+		return $copyright;
+	}
+}
+?>
