03b63942b5d267ab7323337c8ceffb581b3780f4
Helmut Hummel <helmut.hummel@typo3.org>
2014-03-22 22:56:18 +0100

    [TASK] Cleanup Ajax URL generation
    The Ajax URL generation is cleaned up to
    match the current state in TYPO3 master (6.2)
    
    The compatibility for < 6.2 is still retained.
    
    Related: #57096
    Change-Id: I2f3e2e5080fa05eb7145133d7ff254911118d6ea
    Reviewed-on: https://review.typo3.org/28651
    Reviewed-by: Nico de Haen
    Tested-by: Nico de Haen

--- Resources/Public/jsDomainModeling/extbaseModeling.js
+++ Resources/Public/jsDomainModeling/extbaseModeling.js
@@ -1,7 +1,7 @@
 var extbaseModeling_wiringEditorLanguage = {
 	parentEl: 'domainModelEditor',
 	languageName: "extbaseModeling",
-	smdUrl: TYPO3.settings.extensionBuilder.wiringEditorSmdEndpointUrl,
+	smdUrl: TYPO3.settings.ajaxUrls['ExtensionBuilder::wiringEditorSmdEndpoint'],
 	layerOptions: {
 	},
 	modules: []
