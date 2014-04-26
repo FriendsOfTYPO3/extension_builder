0d76404e68850626cd081543909313d69e172245
Roland Waldner <mostwantedtypo3@gmail.com>
2014-04-26 21:24:31 +0200

    [CLEANUP] Fix some CGL violations in ext_tables.php
    * comment line length
    * intentions
    
    Resolves: #58241
    Releases: 6.2
    Change-Id: I15afac656a0220826ec9d4d6d6e4c84024a20c3b
    Reviewed-on: https://review.typo3.org/29680
    Reviewed-by: Nico de Haen
    Tested-by: Nico de Haen

--- ext_tables.php
+++ ext_tables.php
@@ -16,19 +16,24 @@ if (TYPO3_MODE === 'BE' && !(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_INSTALL)) {
 		'extensionbuilder',
 		'',
 		array(
-			 'BuilderModule' => 'index,domainmodelling,dispatchRpc',
+			'BuilderModule' => 'index,domainmodelling,dispatchRpc',
 		),
 		array(
-			 'access' => 'user,group',
-			 'icon' => 'EXT:extension_builder/ext_icon.gif',
-			 'labels' => 'LLL:EXT:extension_builder/Resources/Private/Language/locallang_mod.xlf',
+			'access' => 'user,group',
+			'icon' => 'EXT:extension_builder/ext_icon.gif',
+			'labels' => 'LLL:EXT:extension_builder/Resources/Private/Language/locallang_mod.xlf',
 		)
 	);
 
-	//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerAjaxHandler('ExtensionBuilder::wiringEditorSmdEndpoint', 'EBT\ExtensionBuilder\Configuration\ConfigurationManager->getWiringEditorSmd');
-	// To stay compatible with older TYPO3 versions, we register the ajax script the old way
-	// It is also OK to not have this Ajax call to be CSRF protected as it is of no use for an attacker in this scenario even
-	// if the result contains the module token
-	$GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['ExtensionBuilder::wiringEditorSmdEndpoint'] = 'EBT\ExtensionBuilder\Configuration\ConfigurationManager->getWiringEditorSmd';
+	// \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerAjaxHandler(
+	//     'ExtensionBuilder::wiringEditorSmdEndpoint',
+	//	   'EBT\ExtensionBuilder\Configuration\ConfigurationManager->getWiringEditorSmd'
+	// );
+	// To stay compatible with older TYPO3 versions, we register the ajax script the
+	// old way. It is also OK to not have this Ajax call to be CSRF protected as it
+	// is of no use for an attacker in this scenario even if the result contains the
+	// module token.
+	$GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['ExtensionBuilder::wiringEditorSmdEndpoint'] = '' .
+		'EBT\ExtensionBuilder\Configuration\ConfigurationManager->getWiringEditorSmd';
 
 }
