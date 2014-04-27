410ddcc06e6df7124ac4bfaef94c04e5cd1df770
Roland Waldner <mostwantedtypo3@gmail.com>
2014-04-27 19:09:00 +0200

    [CLEANUP] Cleanup \EBT\ExtensionBuilder\Domain\Model\BackendModule
    Resolves: #58273
    Releases: 6.2
    Change-Id: Icd5c6ea01a02a9299993e915c1b58966ba324d1d
    Reviewed-on: https://review.typo3.org/29693
    Reviewed-by: Nico de Haen
    Tested-by: Nico de Haen

--- Classes/Domain/Model/BackendModule.php
+++ Classes/Domain/Model/BackendModule.php
@@ -24,27 +24,19 @@ namespace EBT\ExtensionBuilder\Domain\Model;
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
-/**
- * A plugin in the extension
- */
 class BackendModule {
+
 	/**
-	 * The name of the module
-	 *
 	 * @var string
 	 */
 	protected $name = '';
 
 	/**
-	 * The description of the module
-	 *
 	 * @var string
 	 */
 	protected $description = '';
 
 	/**
-	 * The tab label
-	 *
 	 * @var string
 	 */
 	protected $tabLabel = '';
@@ -57,14 +49,13 @@ class BackendModule {
 	protected $mainModule = 'web';
 
 	/**
-	 * The module key
-	 *
 	 * @var string
 	 */
 	protected $key = '';
 
 	/**
 	 * array with configuration arrays
+	 *
 	 * array('controller' => 'MyController', 'actions' => 'action1,action2')
 	 *
 	 * @var string[]
@@ -72,8 +63,6 @@ class BackendModule {
 	protected $controllerActionCombinations = array();
 
 	/**
-	 * Gets the Name
-	 *
 	 * @return string
 	 */
 	public function getName() {
@@ -81,8 +70,6 @@ class BackendModule {
 	}
 
 	/**
-	 * Sets the Name
-	 *
 	 * @param string $name
 	 * @return void
 	 */
@@ -90,10 +77,7 @@ class BackendModule {
 		$this->name = $name;
 	}
 
-
 	/**
-	 * Gets the Description
-	 *
 	 * @return string
 	 */
 	public function getDescription() {
@@ -101,8 +85,6 @@ class BackendModule {
 	}
 
 	/**
-	 * Sets the Description
-	 *
 	 * @param string $description
 	 * @return void
 	 */
@@ -111,8 +93,6 @@ class BackendModule {
 	}
 
 	/**
-	 * Gets the tab label
-	 *
 	 * @return string
 	 */
 	public function getTabLabel() {
@@ -120,9 +100,7 @@ class BackendModule {
 	}
 
 	/**
-	 * Sets the tab label
-	 *
-	 * @param string $tablLabel
+	 * @param string $tabLabel
 	 * @return void
 	 */
 	public function setTabLabel($tabLabel) {
@@ -130,10 +108,6 @@ class BackendModule {
 	}
 
 	/**
-	 * Setter for mainModule
-	 *	 /**
-	 * Setter for key
-	 *
 	 * @param string $key
 	 * @return void
 	 */
@@ -142,8 +116,6 @@ class BackendModule {
 	}
 
 	/**
-	 * Getter for key
-	 *
 	 * @return string key
 	 */
 	public function getKey() {
@@ -159,8 +131,7 @@ class BackendModule {
 	}
 
 	/**
-	 * Getter for mainModule
-	 *
+	 * @return string
 	 */
 	public function getMainModule() {
 		return $this->mainModule;
@@ -168,6 +139,7 @@ class BackendModule {
 
 	/**
 	 * @param array $controllerActionCombinations
+	 * @return void
 	 */
 	public function setControllerActionCombinations(array $controllerActionCombinations) {
 		$this->controllerActionCombinations = $controllerActionCombinations;
@@ -179,4 +151,5 @@ class BackendModule {
 	public function getControllerActionCombinations() {
 		return $this->controllerActionCombinations;
 	}
+
 }
