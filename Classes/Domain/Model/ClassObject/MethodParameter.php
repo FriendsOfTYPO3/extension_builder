FriendsOfTYPO3/extension_builder

commit 84b06e7e123eb4468e5948789e971f0e3c983430

[CLEANUP] Cleanup various domain model classes
* import classes
* remove superfluous comments
  * reason explained here: http://forge.typo3.org/issues/58248#note-1
* use spaces in string concatinations
* Improve some vertical rhythm by using some vertical space
* Support method chaining not only for some of the methods in a class
* remove superfluous empty lines

Resolves: #58256
Releases: 6.2
Change-Id: I47c0bc9d4d12b1fcc8e0a96f5c74245e4e732e36
Reviewed-on: https://review.typo3.org/29686
Reviewed-by: Nico de Haen
Tested-by: Nico de Haen

Classes/Domain/Model/ClassObject/MethodParameter.php
@@ -24,11 +24,11 @@ namespace EBT\ExtensionBuilder\Domain\Model\ClassObject;
  ***************************************************************/
 
 /**
- * parameter representing a method parameter in
- * the context of software development
- *
+ * parameter representing a method parameter in the context of software
+ * development
  */
 class MethodParameter extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject{
+
 	/**
 	 * @var string
 	 */
@@ -75,17 +75,13 @@ class MethodParameter extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject{
 	protected $passedByReference = FALSE;
 
 	/**
-	 * __construct
-	 *
-	 * @return void
+	 * @param string $name
 	 */
 	public function __construct($name) {
 		$this->name = $name;
 	}
 
-
 	/**
-	 *
 	 * @return string $name
 	 */
 	public function getName() {
@@ -93,8 +89,7 @@ class MethodParameter extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject{
 	}
 
 	/**
-	 * Returns $varType.
-	 *
+	 * @return string
 	 */
 	public function getVarType() {
 		if (empty($this->varType) && !empty($this->typeHint)) {
@@ -104,17 +99,14 @@ class MethodParameter extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject{
 	}
 
 	/**
-	 * Sets $varType.
-	 *
 	 * @param string $varType
+	 * @return void
 	 */
 	public function setVarType($varType) {
 		$this->varType = $varType;
-		return $this;
 	}
 
 	/**
-	 *
 	 * @return int $position
 	 */
 	public function getPosition() {
@@ -122,8 +114,6 @@ class MethodParameter extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject{
 	}
 
 	/**
-	 * setter for position
-	 *
 	 * @param int $position
 	 * @return void
 	 */
@@ -131,9 +121,7 @@ class MethodParameter extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject{
 		$this->position = $position;
 	}
 
-
 	/**
-	 * getter for defaultValue
 	 * @return mixed
 	 */
 	public function getDefaultValue() {
@@ -141,26 +129,22 @@ class MethodParameter extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject{
 	}
 
 	/**
-	 * setter for defaultValue
-	 * @param $defaultValue
-	 * @return $this
+	 * @param mixed $defaultValue
+	 * @return void
 	 */
 	public function setDefaultValue($defaultValue = NULL) {
 		$this->defaultValue = $defaultValue;
-		return $this;
 	}
 
 	/**
-	 *
-	 * @return boolean
+	 * @return bool
 	 */
 	public function isOptional() {
 		return $this->optional;
 	}
 
 	/**
-	 *
-	 * @param $optional
+	 * @param bool $optional
 	 * @return void
 	 */
 	public function setOptional($optional) {
@@ -168,44 +152,40 @@ class MethodParameter extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject{
 	}
 
 	/**
-	 *
-	 * @return boolean
+	 * @return bool
 	 */
 	public function isPassedByReference() {
 		return $this->passedByReference;
 	}
 
 	/**
-	 * @return boolean
+	 * @return bool
 	 */
 	public function getPassedByReference() {
 		return $this->passedByReference;
 	}
 
 	/**
-	 * @param boolean $passedByReference
+	 * @param bool $passedByReference
+	 * @return void
 	 */
 	public function setPassedByReference($passedByReference) {
 		$this->passedByReference = $passedByReference;
 	}
 
 	/**
-	 *
-	 * @return
+	 * @return string
 	 */
 	public function getTypeHint() {
 		return $this->typeHint;
 	}
 
 	/**
-	 * Sets $typeHint.
-	 *
 	 * @param string $typeHint
-	 * @return $this
+	 * @return void
 	 */
 	public function setTypeHint($typeHint) {
 		$this->typeHint = $typeHint;
-		return $this;
 	}
 
 	/**
@@ -217,13 +197,14 @@ class MethodParameter extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject{
 
 	/**
 	 * @param string $typeForParamTag
+	 * @return void
 	 */
 	public function setTypeForParamTag($typeForParamTag) {
 		$this->typeForParamTag = $typeForParamTag;
 	}
 
 	/**
-	 * @return string|null
+	 * @return string
 	 */
 	public function getTypeForParamTag() {
 		return $this->typeForParamTag;
@@ -231,6 +212,7 @@ class MethodParameter extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject{
 
 	/**
 	 * @param int $startLine
+	 * @return void
 	 */
 	public function setStartLine($startLine) {
 		$this->startLine = $startLine;
@@ -245,6 +227,7 @@ class MethodParameter extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject{
 
 	/**
 	 * @param int $endLine
+	 * @return void
 	 */
 	public function setEndLine($endLine) {
 		$this->endLine = $endLine;

Classes/Domain/Model/ClassObject/Comment.php
@@ -25,6 +25,7 @@ namespace EBT\ExtensionBuilder\Domain\Model\ClassObject;
  ***************************************************************/
 
 class Comment {
+
 	/**
 	 * the raw comment content
 	 *
@@ -37,13 +38,10 @@ class Comment {
 	 */
 	protected $line = -1;
 
-
 	/**
-	  * Constructs a comment node.
-	  *
-	  * @param string $text Comment text (including comment delimiters like /*)
-	  * @param int    $line Line number the comment started on
-	  */
+	 * @param string $text Comment text (including comment delimiters like /*)
+	 * @param int    $line Line number the comment started on
+	 */
 	public function __construct($text, $line = -1) {
 		$this->text = $text;
 		$this->line = $line;
@@ -51,6 +49,7 @@ class Comment {
 
 	/**
 	 * @param string $text
+	 * @return void
 	 */
 	public function setText($text) {
 		$this->text = $text;

Classes/Domain/Model/ClassObject/DocComment.php
@@ -23,11 +23,10 @@ namespace EBT\ExtensionBuilder\Domain\Model\ClassObject;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-/**
- * Class DocComment
- */
 class DocComment extends Comment {
+
 	/**
 	 * @var string
 	 */
@@ -44,21 +43,19 @@ class DocComment extends Comment {
 	protected $tags = array();
 
 	/**
-	  * Constructs a comment node.
-	  *
-	  * @param string $text Comment text (including comment delimiters like /*)
-	  */
+	 * @param string $text Comment text (including comment delimiters like /*)
+	 */
 	public function __construct($text) {
 		$this->text = $text;
 		$this->initialize($text);
 	}
 
 	/**
-	 * Parses the given doc comment and saves the result (description and
-	 * tags) in the parser's object. They can be retrieved by the
-	 * getTags() getTagValues() and getDescription() methods.
+	 * Parses the given doc comment and saves the result (description and tags) in
+	 * the parser's object. They can be retrieved by the getTags() getTagValues()
+	 * and getDescription() methods.
 	 *
-	 * @param string $docComment A doc comment
+	 * @param string $docComment
 	 * @return void
 	 */
 	public function initialize($docComment) {
@@ -74,7 +71,7 @@ class DocComment extends Comment {
 				}
 			}
 		}
-		$this->descriptionLines = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(chr(10), $this->description, TRUE);
+		$this->descriptionLines = GeneralUtility::trimExplode(chr(10), $this->description, TRUE);
 		$this->description = trim($this->description);
 	}
 
@@ -87,10 +84,11 @@ class DocComment extends Comment {
 
 	/**
 	 * @param string $description
+	 * @return void
 	 */
 	public function setDescription($description) {
 		$this->description = $description;
-		//TODO: enable automated splitting into lines after certain number of  characters?
+		// TODO: enable automated splitting into lines after certain number of characters?
 		$this->descriptionLines = array();
 	}
 
@@ -103,27 +101,26 @@ class DocComment extends Comment {
 
 	/**
 	 * @param array $tags
+	 * @return void
 	 */
 	public function setTags(array $tags) {
 		$this->tags = $tags;
 	}
 
 	/**
-	 * sets a tags
-	 *
 	 * @param string $tagName
-	 * @param mixed $tagValue (optional)
+	 * @param mixed $tagValue
+	 * @param bool $override
 	 * @return void
 	 */
-	public function setTag($tagName, $tagValue, $override) {
+	public function setTag($tagName, $tagValue = NULL, $override = FALSE) {
 		if (!$override && isset($this->tags[$tagName])) {
 			if (!is_array($this->tags[$tagName])) {
 				// build an array with the existing value as first element
 				$this->tags[$tagName] = array($this->tags[$tagName]);
 			}
 			$this->tags[$tagName][] = $tagValue;
-		}
-		else {
+		} else {
 			$this->tags[$tagName] = $tagValue;
 		}
 	}
@@ -136,11 +133,11 @@ class DocComment extends Comment {
 	}
 
 	/**
-	 * Returns the values of the specified tag. The doc comment
-	 * must be parsed with parseDocComment() before tags are
-	 * available.
+	 * Returns the values of the specified tag. The doc comment must be parsed with
+	 * parseDocComment() before tags are available.
 	 *
 	 * @param string $tagName The tag name to retrieve the values for
+	 * @throws \InvalidArgumentException
 	 * @return array The tag's values
 	 */
 	public function getTagValues($tagName) {
@@ -151,7 +148,7 @@ class DocComment extends Comment {
 	}
 
 	/**
-	 * unsets a tags
+	 * unsets a tag
 	 *
 	 * @param string $tagName
 	 * @return void
@@ -172,8 +169,8 @@ class DocComment extends Comment {
 	}
 
 	/**
-	 * Parses a line of a doc comment for a tag and its value.
-	 * The result is stored in the internal tags array.
+	 * Parses a line of a doc comment for a tag and its value. The result is stored
+	 * in the internal tags array.
 	 *
 	 * @param string $line A line of a doc comment which starts with an @-sign
 	 * @return void
@@ -185,7 +182,7 @@ class DocComment extends Comment {
 		} else {
 			array_shift($tagAndValue);
 		}
-		$tag = trim($tagAndValue[0].$tagAndValue[1], '@');
+		$tag = trim($tagAndValue[0] . $tagAndValue[1], '@');
 		if (count($tagAndValue) > 1) {
 			$this->tags[$tag][] = trim($tagAndValue[2], ' "');
 		} else {
@@ -196,6 +193,7 @@ class DocComment extends Comment {
 
 	/**
 	 * @param string $value
+	 * @return void
 	 */
 	public function setValue($value) {
 		$this->initialize($value);
@@ -216,6 +214,7 @@ class DocComment extends Comment {
 	 */
 	public function toString($singleLineCommentAllowed = FALSE) {
 		$docCommentLines = array();
+
 		if (is_array($this->tags)) {
 			if (isset($this->tags['return'])) {
 				$returnTagValue = $this->tags['return'];
@@ -235,6 +234,7 @@ class DocComment extends Comment {
 				}
 			}
 		}
+
 		if (!empty($this->description)) {
 			array_unshift($docCommentLines, PHP_EOL);
 			if (!empty($this->descriptionLines)) {
@@ -243,6 +243,7 @@ class DocComment extends Comment {
 				array_unshift($docCommentLines, $this->description);
 			}
 		}
+
 		if ($singleLineCommentAllowed && count($docCommentLines) === 1) {
 			return '/** ' . $docCommentLines[0] . ' */';
 		} else {
@@ -251,4 +252,5 @@ class DocComment extends Comment {
 			return '/**' . PHP_EOL . implode(PHP_EOL, $docCommentLines) . PHP_EOL . ' */';
 		}
 	}
+
 }

Classes/Domain/Model/ClassObject/Method.php
@@ -22,11 +22,11 @@ namespace EBT\ExtensionBuilder\Domain\Model\ClassObject;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
+use EBT\ExtensionBuilder\Domain\Model\FunctionObject;
 
 /**
  * method representing a "method" in the context of software development
- *
  */
-class Method extends \EBT\ExtensionBuilder\Domain\Model\FunctionObject {
+class Method extends FunctionObject {
 
 }

Classes/Domain/Model/ClassObject/Property.php
@@ -22,26 +22,20 @@ namespace EBT\ExtensionBuilder\Domain\Model\ClassObject;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
+use EBT\ExtensionBuilder\Domain\Model\AbstractObject;
+use EBT\ExtensionBuilder\Domain\Model\DomainObject;
 
 /**
  * property representing a "property" in the context of software development
  */
-class Property extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject {
+class Property extends AbstractObject {
+
 	/**
 	 * PHP var type of this property (read from "@var" annotation in doc comment)
 	 *
 	 * @var string
 	 */
 	protected $varType = '';
-
-	/**
-	 * if there is a domain object property associated
-	 * with this ClassProperty this object holds all extbase related information
-	 * (like SQL, TYPO3 related stuff)
-	 *
-	 * @var object associatedDomainObjectProperty
-	 */
-	protected $associatedDomainObjectProperty = NULL;
 
 	/**
 	 * @var mixed
@@ -54,29 +48,23 @@ class Property extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject {
 	protected $value = NULL;
 
 	/**
-	 * in case of properties of type array
-	 * we need to preserve the parsed statements
-	 * to be able to reapply the original
-	 * linebrakes
+	 * In case of properties of type array we need to preserve the parsed statements
+	 * to be able to reapply the original linebrakes.
 	 *
 	 * @var \PHPParser_NodeAbstract
 	 */
 	protected $defaultValueNode = NULL;
 
 	/**
-	 * __construct
-	 *
+	 * @param string $name
 	 * @param string
-	 * @return void
 	 */
 	public function __construct($name) {
 		$this->name = $name;
 	}
 
 	/**
-	 * Setter for name
-	 *
-	 * @param string $name name
+	 * @param string $name
 	 * @return Property
 	 */
 	public function setName($name) {
@@ -85,55 +73,23 @@ class Property extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject {
 	}
 
 	/**
-	 *
-	 * @return string $type.
+	 * @return string
 	 */
 	public function getVarType() {
 		return $this->varType;
 	}
 
 	/**
-	 * Sets $type.
-	 *
-	 * @param string $type
+	 * @param string $varType
+	 * @return void
 	 */
 	public function setVarType($varType) {
 		$this->setTag('var', array($varType));
 		$this->varType = $varType;
 	}
 
-
 	/**
-	 * Returns $associatedDomainObjectProperty.
-	 *
-	 * @return object associatedDomainObjectProperty
-	 */
-	public function getAssociatedDomainObjectProperty() {
-		return $this->associatedDomainObjectProperty;
-	}
-
-	/**
-	 * Sets $associatedDomainObjectProperty.
-	 *
-	 * @param object $associatedDomainObjectProperty
-	 */
-	public function setAssociatedDomainObjectProperty($associatedDomainObjectProperty) {
-		$this->associatedDomainObjectProperty = $associatedDomainObjectProperty;
-		if (empty($this->description)) {
-			$this->description = $associatedDomainObjectProperty->getDescription();
-			if (empty($this->description)) {
-				$this->description = $this->name;
-			}
-		}
-	}
-
-	public function hasAssociatedDomainObjectProperty() {
-		return !is_null($this->associatedDomainObjectProperty);
-	}
-
-	/**
-	 *
-	 * @return boolean
+	 * @return bool
 	 */
 	public function isDefault() {
 		return $this->default;
@@ -141,7 +97,6 @@ class Property extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject {
 
 	/**
 	 * @param mixed $default
-	 *
 	 * @return void
 	 */
 	public function setDefault($default) {
@@ -163,17 +118,17 @@ class Property extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject {
 	}
 
 	/**
-	 * Setter for value
-	 *
-	 * @param mixed
+	 * @param mixed $value
+	 * @return void
 	 */
 	public function setValue($value) {
 		$this->value = $value;
 	}
 
 	/**
-	 * This is a helper function to be called in fluid if conditions
-	 * it returns TRUE even if the default value is 0 or an empty string or "FALSE"
+	 * This is a helper function to be called in fluid if conditions it returns TRUE
+	 * even if the default value is 0 or an empty string or "FALSE".
+	 *
 	 * @return bool
 	 */
 	public function getHasDefaultValue() {
@@ -184,8 +139,9 @@ class Property extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject {
 	}
 
 	/**
-	 * This is a helper function to be called in fluid if conditions
-	 * it returns TRUE even if the value is 0 or an empty string or "FALSE"
+	 * This is a helper function to be called in fluid if conditions it returns TRUE
+	 * even if the value is 0 or an empty string or "FALSE".
+	 *
 	 * @return bool
 	 */
 	public function getHasValue() {
@@ -195,16 +151,15 @@ class Property extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject {
 		return FALSE;
 	}
 
-
 	/**
 	 * @param \PHPParser_NodeAbstract $defaultValueNode
+	 * @return void
 	 */
 	public function setDefaultValueNode($defaultValueNode) {
 		$this->defaultValueNode = $defaultValueNode;
 	}
 
 	/**
-	 *
 	 * @return \PHPParser_NodeAbstract
 	 */
 	public function getDefaultValueNode() {

commit 6398e6a50f184482366fd48931b7c559759cf47d

[BUGFIX] Re-add two @return $this annotations
This is a regression bug introduced with commit
84b06e7e123 which solves #58256.

At the moment unit tests are red.

Resolves: #58304
Releases: 6.2
Change-Id: Ibe10365a5f1232cc58fc6e4a56a2d915c9ac43f8
Reviewed-on: https://review.typo3.org/29720
Reviewed-by: Oliver Klee
Reviewed-by: Nico de Haen
Tested-by: Nico de Haen

Classes/Domain/Model/ClassObject/MethodParameter.php
@@ -100,10 +100,11 @@ class MethodParameter extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject{
 
 	/**
 	 * @param string $varType
-	 * @return void
+	 * @return \EBT\ExtensionBuilder\Domain\Model\ClassObject\MethodParameter This method is used in fluent interfaces!
 	 */
 	public function setVarType($varType) {
 		$this->varType = $varType;
+		return $this;
 	}
 
 	/**
@@ -182,10 +183,11 @@ class MethodParameter extends \EBT\ExtensionBuilder\Domain\Model\AbstractObject{
 
 	/**
 	 * @param string $typeHint
-	 * @return void
+	 * @return \EBT\ExtensionBuilder\Domain\Model\ClassObject\MethodParameter This method is used in fluent interfaces!
 	 */
 	public function setTypeHint($typeHint) {
 		$this->typeHint = $typeHint;
+		return $this;
 	}
 
 	/**