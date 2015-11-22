1e1396feb910ef524587746b90021fb01c9484be
Nico de Haen <mail@ndh-websolutions.de>
2015-11-23 00:04:22 +0100

    Use correct format for TYPO3 dependency
    Releases: 7.6
    Change-Id: I7dadefe7eb2f321adc532f4597cbde88b9936a77
    Reviewed-on: https://review.typo3.org/44908
    Reviewed-by: Nico de Haen <mail@ndh-websolutions.de>
    Tested-by: Nico de Haen <mail@ndh-websolutions.de>

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '7.6.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.6.*'
+			'typo3' => '7.6.0-7.6.99'
 		),
 		'conflicts' => array(
 		),
