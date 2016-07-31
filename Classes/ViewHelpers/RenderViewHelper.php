d02877e1e377df4503b74ad5cc18e4ace5c2686e
Nico de Haen <mail@ndh-websolutions.de>
2016-07-31 13:25:45 +0200

    Set escapeChildren=false with custom RenderViewHelper
    Resolves #77316
    Releases 8.0
    
    Change-Id: Ie97661fd92ac8011778c6a574875f201b8e6b3d5

---
+++ Classes/ViewHelpers/RenderViewHelper.php
@@ -0,0 +1,21 @@
+<?php
+namespace EBT\ExtensionBuilder\ViewHelpers;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+class RenderViewHelper extends \TYPO3Fluid\Fluid\ViewHelpers\RenderViewHelper {
+
+    protected $escapeChildren = FALSE;
+
+}
