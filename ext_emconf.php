18e0a79b64563336f0329910846ea52abe844cb5
Markus Klein <markus.klein@typo3.org>
2017-01-17 13:40:52 +0100

    [TASK] Run tests with PHP 7.x as well
    Releases: master, 7.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,6 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '7.6.0',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.5.0-7.0.99',
             'typo3' => '7.6.0-7.6.99'
         ),
         'conflicts' => array(
