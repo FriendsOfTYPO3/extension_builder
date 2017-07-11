a2e802e1f7a0b292cd2ab831f22968c9c0f76b39
Nico de Haen <mail@ndh-websolutions.de>
2017-07-11 08:30:21 +0200

    Add TCA type email
    Resolves #50

---
+++ Classes/Domain/Model/DomainObject/EmailProperty.php
@@ -0,0 +1,40 @@
+<?php
+namespace EBT\ExtensionBuilder\Domain\Model\DomainObject;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+class EmailProperty extends AbstractProperty
+{
+    /**
+     * the property's default value
+     *
+     * @var string
+     */
+    protected $defaultValue = '';
+
+    public function getTypeForComment()
+    {
+        return 'string';
+    }
+
+    public function getTypeHint()
+    {
+        return '';
+    }
+
+    public function getSqlDefinition()
+    {
+        return $this->getFieldName() . " varchar(255) DEFAULT '' NOT NULL,";
+    }
+}
---
+++ Resources/Private/CodeTemplates/Extbase/Partials/TCA/EmailProperty.phpt
@@ -0,0 +1,5 @@
+[
+    'type' => 'input',
+    'size' => 30,
+    'eval' => 'nospace,email<f:if condition="{property.required}">,required</f:if>'
+]
