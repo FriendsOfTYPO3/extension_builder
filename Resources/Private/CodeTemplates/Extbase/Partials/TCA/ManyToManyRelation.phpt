7bc7733cec5e54d25f2184bfb3959184e0456151
Stephan Bauer <stephan_bauer@gmx.de>
2017-10-02 23:45:56 +0200

    Fix deprecation in ManyToMany

--- Resources/Private/CodeTemplates/Extbase/Partials/TCA/ManyToManyRelation.phpt
+++ Resources/Private/CodeTemplates/Extbase/Partials/TCA/ManyToManyRelation.phpt
@@ -2,36 +2,20 @@
     'type' => 'select',
     'renderType' => '{property.renderType}',
     'foreign_table' => '{property.foreignDatabaseTableName}',
-    'MM' => '{property.relationTableName}',
+    'MM' => '{property.relationTableName}',<f:if condition="{property.renderType} == 'selectMultipleSideBySide'">
     'size' => 10,
     'autoSizeMax' => 30,
     'maxitems' => 9999,
     'multiple' => 0,
-    'wizards' => [
-        '_PADDING' => 1,
-        '_VERTICAL' => 1,
-        'edit' => [
-            'module' => [
-                'name' => 'wizard_edit',
-            ],
-            'type' => 'popup',
-            'title' => 'Edit', // todo define label: LLL:EXT:.../Resources/Private/Language/locallang_tca.xlf:wizard.edit
-            'icon' => 'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_edit.gif',
-            'popup_onlyOpenIfSelected' => 1,
-            'JSopenParams' => 'height=350,width=580,status=0,menubar=0,scrollbars=1',
+    'fieldControl' => [
+        'editPopup' => [
+            'disabled' => false,
         ],
-        'add' => [
-            'module' => [
-                'name' => 'wizard_add',
-            ],
-            'type' => 'script',
-            'title' => 'Create new', // todo define label: LLL:EXT:.../Resources/Private/Language/locallang_tca.xlf:wizard.add
-            'icon' => 'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_add.gif',
-            'params' => [
-                'table' => '{property.foreignDatabaseTableName}',
-                'pid' => '###CURRENT_PID###',
-                'setValue' => 'prepend'
-            ],
+        'addRecord' => [
+            'disabled' => false,
         ],
-    ],
+        'listModule' => [
+            'disabled' => true,
+        ],
+    ],</f:if>
 ],
--- Tests/Fixtures/TestExtensions/test_extension/Configuration/TCA/tx_testextension_domain_model_main.php
+++ Tests/Fixtures/TestExtensions/test_extension/Configuration/TCA/tx_testextension_domain_model_main.php
@@ -227,31 +227,15 @@ return [
                 'autoSizeMax' => 30,
                 'maxitems' => 9999,
                 'multiple' => 0,
-                'wizards' => [
-                    '_PADDING' => 1,
-                    '_VERTICAL' => 1,
-                    'edit' => [
-                        'module' => [
-                            'name' => 'wizard_edit',
-                        ],
-                        'type' => 'popup',
-                        'title' => 'Edit', // todo define label: LLL:EXT:.../Resources/Private/Language/locallang_tca.xlf:wizard.edit
-                        'icon' => 'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_edit.gif',
-                        'popup_onlyOpenIfSelected' => 1,
-                        'JSopenParams' => 'height=350,width=580,status=0,menubar=0,scrollbars=1',
+                'fieldControl' => [
+                    'editPopup' => [
+                        'disabled' => false,
                     ],
-                    'add' => [
-                        'module' => [
-                            'name' => 'wizard_add',
-                        ],
-                        'type' => 'script',
-                        'title' => 'Create new', // todo define label: LLL:EXT:.../Resources/Private/Language/locallang_tca.xlf:wizard.add
-                        'icon' => 'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_add.gif',
-                        'params' => [
-                            'table' => 'tx_testextension_domain_model_child4',
-                            'pid' => '###CURRENT_PID###',
-                            'setValue' => 'prepend'
-                        ],
+                    'addRecord' => [
+                        'disabled' => false,
+                    ],
+                    'listModule' => [
+                        'disabled' => true,
                     ],
                 ],
             ],
