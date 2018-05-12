319d0be17943698832521b9fc0be5185dd2fd558
Nico de Haen <mail@ndh-websolutions.de>
2018-05-12 16:58:14 +0200

    Remove obsolete mail test

--- Tests/Fixtures/TestExtensions/test_extension/Tests/Unit/Domain/Model/Child1Test.php
+++ Tests/Fixtures/TestExtensions/test_extension/Tests/Unit/Domain/Model/Child1Test.php
@@ -74,28 +74,4 @@ class Child1Test extends \TYPO3\CMS\Core\Tests\UnitTestCase
         );
     }
 
-    /**
-     * @test
-     */
-    public function getMailReturnsInitialValueForString()
-    {
-        self::assertSame(
-            '',
-            $this->subject->getMail()
-        );
-    }
-
-    /**
-     * @test
-     */
-    public function setMailForStringSetsMail()
-    {
-        $this->subject->setMail('Conceived at T3CON10');
-
-        self::assertAttributeEquals(
-            'Conceived at T3CON10',
-            'mail',
-            $this->subject
-        );
-}
 }
