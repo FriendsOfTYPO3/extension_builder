9b0f2ed9288407fe6a04adb6b0348f80a0ebdb1a
Oliver Kleinecke <okleinecke@web.de>
2018-06-22 12:41:14 +0200

    Update tx_testextension_domain_model_main.php

--- Tests/Fixtures/TestExtensions/test_extension/Configuration/TCA/tx_testextension_domain_model_main.php
+++ Tests/Fixtures/TestExtensions/test_extension/Configuration/TCA/tx_testextension_domain_model_main.php
@@ -79,7 +79,7 @@ return [
                 'type' => 'check',
                 'items' => [
                     '1' => [
-                        '0' => 'LLL:EXT:lang/locallang_core.xlf:labels.enabled'
+                        '0' => 'LLL:EXT:lang/Resources/Private/Language/locallang_core.xlf:labels.enabled'
                     ]
                 ],
             ],
