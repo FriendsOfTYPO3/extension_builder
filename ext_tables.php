6a78138909b19391517fa049f91ed259c0fb50b2
Nico de Haen <mail@ndh-websolutions.de>
2018-07-04 23:50:54 +0200

    Rename Icon

--- ext_tables.php
+++ ext_tables.php
@@ -15,7 +15,7 @@ if (TYPO3_MODE === 'BE') {
         ),
         array(
             'access' => 'user,group',
-            'icon' => 'EXT:extension_builder/Resources/Public/Icons/module-extensionbuilder.svg',
+            'icon' => 'EXT:extension_builder/Resources/Public/Icons/Extension.svg',
             'labels' => 'LLL:EXT:extension_builder/Resources/Private/Language/locallang_mod.xlf',
         )
     );
