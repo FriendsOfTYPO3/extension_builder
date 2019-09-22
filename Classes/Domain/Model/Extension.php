7ea94230bb5ae0f72e2f7ab8a2d2eccc9cf12e46
Nico de Haen <mail@ndh-websolutions.de>
2019-09-22 18:38:52 +0200

    Remevode version in generated composer.json
    Update version in several places
    
    resolves #194

--- Classes/Domain/Model/Extension.php
+++ Classes/Domain/Model/Extension.php
@@ -894,7 +894,6 @@ class Extension
             'type' => 'typo3-cms-extension',
             'description' => $this->description,
             'authors' => [],
-		    'version' => '1.0.0',
             'require' => [
                 'typo3/cms-core' => '^9.5'
             ],
--- Tests/Fixtures/TestExtensions/test_extension/ext_emconf.php
+++ Tests/Fixtures/TestExtensions/test_extension/ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.9',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '9.5.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
