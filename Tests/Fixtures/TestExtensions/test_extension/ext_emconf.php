438f4470946f51a79829f369f2fe276423a0e1ee
Franz Holzinger <franz.holzinger@festwein.de>
2021-03-09 09:14:54 +0100

    remove old createDirs option from EMCONF (#375)

--- Tests/Fixtures/TestExtensions/test_extension/ext_emconf.php
+++ Tests/Fixtures/TestExtensions/test_extension/ext_emconf.php
@@ -17,7 +17,6 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'John Doe',
     'author_email' => 'mail@typo3.com',
     'state' => 'alpha',
-    'createDirs' => '',
     'clearCacheOnLoad' => 0,
     'version' => '0.9',
     'constraints' => [
