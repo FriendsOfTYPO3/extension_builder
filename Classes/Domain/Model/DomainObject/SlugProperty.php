6f035d1f0f0134518cb48ad526c983d1819f77e7
ste101 <stephan_bauer@gmx.de>
2021-03-12 19:14:59 +0100

    Create SlugProperty.php

---
+++ Classes/Domain/Model/DomainObject/SlugProperty.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace EBT\ExtensionBuilder\Domain\Model\DomainObject;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+class SlugProperty extends AbstractProperty
+{
+    /**
+     * the property's default value
+     *
+     * @var string
+     */
+    protected $defaultValue = '';
+    /**
+     * @var bool
+     */
+    protected $searchable = true;
+
+    public function getTypeForComment(): string
+    {
+        return 'string';
+    }
+
+    public function getTypeHint(): string
+    {
+        return '';
+    }
+
+    public function getSqlDefinition(): string
+    {
+        return $this->getFieldName() . " varchar(255) DEFAULT '' NOT NULL,";
+    }
+}
