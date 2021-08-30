336f843436912ddce17bb9efe6f548a22405ea05
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2021-08-30 17:34:05 +0200

    [TASK] Replace TYPO3_MODE and TYPO3_REQUESTTYPE constants
    Resolves #467

--- Tests/Fixtures/TestExtensions/test_extension/Configuration/TCA/Overrides/sys_template.php
+++ Tests/Fixtures/TestExtensions/test_extension/Configuration/TCA/Overrides/sys_template.php
@@ -1,4 +1,4 @@
 <?php
-defined('TYPO3_MODE') || die();
+defined('TYPO3') || die();
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('test_extension', 'Configuration/TypoScript', 'ExtensionBuilder Test Extension');
