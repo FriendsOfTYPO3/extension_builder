18629e05d2f0c9016f7c67c7475b154169de6a43
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2022-02-10 01:16:50 -0600

    [TASK] Update return type hint for model getters
    Resolves: #566

--- Tests/Fixtures/TestExtensions/test_extension/Classes/Domain/Model/Child4.php
+++ Tests/Fixtures/TestExtensions/test_extension/Classes/Domain/Model/Child4.php
@@ -38,7 +38,7 @@ class Child4 extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
     /**
      * Returns the name
      *
-     * @return string $name
+     * @return string
      */
     public function getName()
     {
@@ -59,7 +59,7 @@ class Child4 extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
     /**
      * Returns the fileProperty
      *
-     * @return \TYPO3\CMS\Extbase\Domain\Model\FileReference $fileProperty
+     * @return \TYPO3\CMS\Extbase\Domain\Model\FileReference
      */
     public function getFileProperty()
     {
