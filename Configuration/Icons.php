6e7ef5d95d5192296589e2e018c152c0e2b450d9
Philipp Kuhlmay <philipp.kuhlmay@wacon.de>
2022-10-02 14:03:43 +0200

    [TASK] #404 - Align with new API for backend modules
    Using new API for registering backend modules
    Register Icon for backend module via new Icons.php

---
+++ Configuration/Icons.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    // Register extensionbuilder module icon
+    'extensionbuilder-module' => [
+        'provider' => TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:extension_builder/Resources/Public/Icons/Extension.svg',
+    ],
+];
