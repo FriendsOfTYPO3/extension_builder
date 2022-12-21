642fb99990a082edfde80773fec943ce4045c0a7
Philipp Kuhlmay <philipp.kuhlmay@wacon.de>
2022-12-21 18:27:27 +0100

    Include Vue JS to backend module and testing button

--- Resources/Public/JavaScript/extensionbuilder.js
+++ Resources/Public/JavaScript/extensionbuilder.js
@@ -1,79 +1,32 @@
-// console.log("Hello from extensionbuilder.js");
 define([
   'jquery',
   'TYPO3/CMS/ExtensionBuilder/Contrib/vue',
   'TYPO3/CMS/Backend/Notification',
   'TYPO3/CMS/Backend/Modal'
-], function ($, Vue, Notification, Modal) {
+], function ($, Vue, Notification, Modal, App, createApp) {
+
+  /* Check if tag with id #modeller is available */
   if (!document.getElementById('modeller')) {
+    // If not, return
     return;
   }
 
-  new Vue({
+  // const app = createApp(App);
+
+  // console.log(app);
+  const vue = new Vue({
     el: '#modeller',
-    data: function() {
-      return {
-        mode: 'overview',
-        extension: [
-          {
-            title: "Create extension",
-            bodytext: "With this tool you can create a whole extension including viewhelper, widgets and scheduler tasks.",
-            linktext: "Create extension",
-            linkhref: "ExtensionPage",
-            linkenabled: true,
-            buttonstyle: "btn btn-success",
-            icon: "content-extension",
-            footertext: ""
-          },
-          {
-            title: "Create schuduler task",
-            bodytext: "Sed lectus. Vestibulum suscipit nulla quis orci. Praesent congue erat at massa. Vestibulum suscipit nulla quis orci. Suspendisse feugiat.",
-            linktext: "Create scheduler task",
-            linkhref: "SchedulerTaskPage",
-            linkenabled: false,
-            buttonstyle: "btn btn-danger",
-            icon: "mimetypes-x-tx_scheduler_task_group",
-            footertext: "Coming soon"
-          },
-          {
-            title: "Create viewhelper",
-            bodytext: "Sed lectus. Vestibulum suscipit nulla quis orci. Praesent congue erat at massa. Vestibulum suscipit nulla quis orci. Suspendisse feugiat.",
-            linktext: "Create viewhelper",
-            linkhref: "ViewhelperPage",
-            linkenabled: false,
-            buttonstyle: "btn btn-danger",
-            icon: "module-viewpage",
-            footertext: "Coming soon"
-          },
-          {
-            title: "Create widgets",
-            bodytext: "Sed lectus. Vestibulum suscipit nulla quis orci. Praesent congue erat at massa. Vestibulum suscipit nulla quis orci. Suspendisse feugiat.",
-            linktext: "Create widgets",
-            linkhref: "WidgetsPage",
-            linkenabled: false,
-            buttonstyle: "btn btn-danger",
-            icon: "content-widget-chart-pie",
-            footertext: "Coming soon"
-          }
-        ],
-      }
-    },
     methods: {
-      changeActiveView(componentName) {
-        console.log("This should change the active view");
-        console.log(componentName);
-      }
-    },
-    // mutations: {
-    //   setActiveView(view) {
-    //     state.activeView = view;
-    //   }
-    // },
-    computed: {
-      activeView() {
-        console.log("Active View()");
+      consoleTest: function() {
+        const today = new Date();
+        const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        const dateTime = date +' '+ time;
+        console.log(dateTime);
       }
     }
   });
-  console.log(Vue);
+
+  // TEMP TODO: remove
+  console.log(vue);
 });
