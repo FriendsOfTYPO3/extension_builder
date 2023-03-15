dd5fed5d8646ba3a12df0aa504d3ead60f048d55
Philipp Kuhlmay <philipp.kuhlmay@wacon.de>
2023-03-15 20:31:44 +0100

    [TASK] #401 - Raise new versions for new TYPO3 12 release
    # Conflicts:
    #	.github/workflows/tests.yml
    #	composer.json
    #	ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,11 +14,11 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'module',
     'author' => 'Extension Builder Team',
     'author_email' => '',
-    'state' => 'stable',
-    'version' => '11.0.13',
+    'state' => 'beta',
+    'version' => '12.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.4.0-11.9.99'
+            'typo3' => '12.2.0-12.9.99'
         ],
         'conflicts' => [],
         'suggests' => []
