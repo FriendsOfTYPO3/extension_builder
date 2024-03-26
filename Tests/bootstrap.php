314d95ac0f21276e23ddc3dc94f0e188eb3eaa08
Philipp Kuhlmay <philipp.kuhlmay@wacon.de>
2024-03-26 16:27:27 +0100

    update phpunit and others

---
+++ Tests/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "extension_builder" extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+require __DIR__ . '/../.Build/vendor/autoload.php';
+
+// Needed for PageRenderer mock
+// defined('LF') ?: define('LF', chr(10));
