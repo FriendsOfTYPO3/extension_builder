7ab5e903922bf631f20d9105ecc05553464d1498
DavidBruchmann <david.bruchmann@gmail.com>
2024-04-05 01:17:23 +0700

    compatibility 12 & 13, re-add composer.json

--- Tests/Fixtures/TestExtensions/test_extension/ext_localconf.php
+++ Tests/Fixtures/TestExtensions/test_extension/ext_localconf.php
@@ -1,7 +1,6 @@
 <?php
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 use FIXTURE\TestExtension\Controller\MainController;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 defined('TYPO3') || die();
 
 (static function() {
@@ -15,25 +14,5 @@ defined('TYPO3') || die();
         [
             MainController::class => 'create, update, delete, '
         ]
-    );
-
-    // wizards
-    ExtensionManagementUtility::addPageTSConfig(
-        'mod {
-            wizards.newContentElement.wizardItems.plugins {
-                elements {
-                    testplugin {
-                        iconIdentifier = test_extension-plugin-testplugin
-                        title = LLL:EXT:test_extension/Resources/Private/Language/locallang_db.xlf:tx_test_extension_testplugin.name
-                        description = LLL:EXT:test_extension/Resources/Private/Language/locallang_db.xlf:tx_test_extension_testplugin.description
-                        tt_content_defValues {
-                            CType = list
-                            list_type = testextension_testplugin
-                        }
-                    }
-                }
-                show = *
-            }
-       }'
     );
 })();
