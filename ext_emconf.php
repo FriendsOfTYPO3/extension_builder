b581c906d427265635bfe68eeae9345447b83915
DavidBruchmann <david.bruchmann@gmail.com>
2024-04-05 02:16:59 +0700

    adjust requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '12.0.0-beta.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.11-12.9.99'
+            'typo3' => '12.4.11-13.0.99'
         ],
         'conflicts' => [],
         'suggests' => []
